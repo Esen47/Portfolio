6fdd3555b74ac6ba9611cc265f4880657f96f837
Sasa Nikolic <sasa.nikolic225@gmail.com>
2020-03-22 15:52:16 -0400

    added links

---
+++ js/main.js
@@ -0,0 +1,69 @@
+let winHeight;
+let footerHeight;
+let sectionClass;
+let sectionPosition;
+let fromTop;
+let navigation = $('.navigation');
+let navHeight = $('.navigation').height();
+let skills = $('.skills-section');
+let intro = $('.intro-content');
+const addForm = $('.form-submit');
+const contactForm = $('.contact-form');
+
+
+$(document).ready(() => {
+    intro.fadeIn(3000);
+});
+
+
+$(window).on('scroll', function() {
+    fromTop = $(window).scrollTop();
+    console.log(fromTop);
+    if(fromTop > 100) {
+        navigation.addClass('resize-navigation');
+    } else {
+        navigation.removeClass('resize-navigation');
+    }
+});
+
+function sectionHeight() {
+    winHeight = $(window).height();
+    console.log(winHeight);
+    $('.intro-section, .skills-section, .projects').css('min-height', winHeight);
+    footerHeight = $('.footer').outerHeight();
+    console.log(footerHeight);
+    $('.contact').css('min-height', winHeight - footerHeight - navHeight);
+}
+
+sectionHeight();
+$(window).on('resize', function() {
+    sectionHeight();
+});
+
+$('.nav-link').on('click',function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+$('nav a').on('click', function(e) {
+    e.preventDefault();
+    sectionClass = $(this).attr('href');
+    console.log(sectionClass);
+    sectionPosition = $(sectionClass).offset().top;
+    console.log(sectionPosition); 
+
+    $('html, body').animate({
+        scrollTop: sectionPosition - navHeight
+    }, 1000);
+});
+
+$('.icon-holder').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+});
+
+$('#intro-btn').on('click', function() {
+    $('html, body').animate({
+        scrollTop: skills.offset().top - navHeight
+    }, 700);
+});
